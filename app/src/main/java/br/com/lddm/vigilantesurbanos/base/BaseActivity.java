fec8f7c3540a14566b586fca3d7b93f3d5ca8099
raianap76 <raianap76@gmail.com>
2019-06-18 10:33:23 -0300

    Initial commit

---
+++ app/src/main/java/br/com/lddm/vigilantesurbanos/base/BaseActivity.java
@@ -0,0 +1,53 @@
+package br.com.lddm.vigilantesurbanos.base;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+/**
+ * @author : Raiana Pereira e Bertone Cândido
+ * Aplicativo desenvolvido com o objetivo que cidadãos comuns reportem incidentes relacionados ao estados
+ * das ruas, iluminação publica, roubos frequentes em determinadas ruas , entre outros
+ */
+
+import br.com.lddm.vigilantesurbanos.auth.LoginActivity;
+
+public class BaseActivity extends AppCompatActivity{
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        mAuth = FirebaseAuth.getInstance();
+        mAuthListener = firebaseAuth -> {
+            FirebaseUser user = firebaseAuth.getCurrentUser();
+            if (user == null) {
+                startActivity(new Intent(getApplicationContext(), LoginActivity.class));
+                finish();
+            }
+        };
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if (mAuthListener != null) {
+            mAuth.removeAuthStateListener(mAuthListener);
+        }
+    }
+
+    protected void signOut() {
+        mAuth.signOut();
+    }
+}
